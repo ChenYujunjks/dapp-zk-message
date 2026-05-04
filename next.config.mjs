5776e9f99c0bc98dcc7fbd92dd7f46ec89f39439
Bruce Chen <aaaeeettt124@126.com>
2026-05-04 06:29:44 -0400

    new

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,16 @@
 const nextConfig = {
   serverExternalPackages: ["snarkjs", "circomlibjs", "ffjavascript"],
 
+  outputFileTracingIncludes: {
+    "/api/generate-proof": [
+      "src/server/zk/circuit/merkle_message.wasm",
+      "src/server/zk/circuit/merkle_message_final.zkey",
+    ],
+  },
+
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
-  },
-
-  // 👇 关键就在这里
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/generate-proof": [
-        "./src/server/zk/circuit/merkle_message.wasm",
-        "./src/server/zk/circuit/merkle_message_final.zkey",
-      ],
-    },
   },
 };
 
